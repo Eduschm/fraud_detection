0400dbca4d565ec2d240f70f36f07c28efd32f4c
Eduardo schmegel <eschmegel@hotmail.com>
2025-11-27 21:00:02 -0300

    feat: added intitial config of streamlit app

---
+++ app/streamlit.py
@@ -0,0 +1,44 @@
+from main import FraudDetectionApp
+import streamlit as st
+
+app_instance = FraudDetectionApp()
+
+# Streamlit config
+st.set_page_config(page_title="Fraud Detection API", layout="centered")
+# Theme
+def set_custom_css():
+    st.markdown(
+        """
+        <style>
+        :root {
+            --primary-color: #1abc9c;
+            --bg-color: #0e1117;
+            --secondary-bg: #111318;
+            --text-color: #e6eef8;
+        }
+        .stApp {
+            background-color: var(--bg-color);
+            color: var(--text-color);
+        }
+        .css-1d391kg { 
+            background-color: var(--secondary-bg) !important;
+        }
+        .stButton>button {
+            background-color: var(--primary-color) !important;
+            color: white !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+# Streamlit interface
+
+
+st.title("Fraud Detection API Status")
+st.write("API is running and ready to accept requests.")
+
+# run app_instance.app.run()
+
+# pip freeze only required
--- src/load_data.py
+++ src/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Load fraud data from CSV and drop isFraud,isFlaggedFraud
 def load_data(file_path):
-    df = pd.read_csv('fraud_data.csv')
+    df = pd.read_csv('data/fraud_data.csv')
     X = df.drop(['isFraud', 'isFlaggedFraud'], axis=1)
     y = df['isFraud']
     return X, y
