7254a928ec0f2abbfa10d3df9d346b7d162c283b
Eduardo schmegel <eschmegel@hotmail.com>
2025-11-27 23:45:15 -0300

    fix -- api

--- app/main.py
+++ app/main.py
@@ -4,24 +4,28 @@ import pandas as pd
 from src.load_data import load_data
 class FraudDetectionApp:
     def __init__(self, query_params: dict = None):
-        self.app = FastAPI()
-        self.sample_data = pd.read_csv('data/sample_data.csv')
+        a = 1 # Placeholder to avoid empty constructor
 
-        @self.app.get("/")
-        def status():
-            return {"status": "ok"}
-
-        @self.app.get("/predict")
-        def predict():
-            return ModelPredictor().evaluate(query_params['X'])
-        
-        @self.app.get("/get_samples")
-        def get_samples():
-            samples = self.sample_data.sample(n=5).to_dict(orient='records')
-            return {"samples": samples}
         
     def predict(self, query_params):
         query_params = pd.DataFrame([query_params])
         X = load_data(query_params, predict=True)
         return ModelPredictor().evaluate(X)
 
+app = FastAPI()
+
+
+sample_data = pd.read_csv('data/sample_data.csv')
+
+@app.get("/")
+def status():
+    return {"status": "ok"}
+
+@app.get("/predict")
+def predict():
+    return ModelPredictor().evaluate(query_params['X'])
+
+@app.get("/get_samples")
+def get_samples():
+    samples = sample_data.sample(n=5).to_dict(orient='records')
+    return {"samples": samples}
