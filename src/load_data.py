10834c65bdb4bcbd89b07ad55437fc25afb21b8b
Eduardo schmegel <eschmegel@hotmail.com>
2025-11-27 21:14:30 -0300

    feat: removed variable fro mdata set

--- src/load_data.py
+++ src/load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Load fraud data from CSV and drop isFraud,isFlaggedFraud
 def load_data(file_path):
     df = pd.read_csv('data/fraud_data.csv')
-    X = df.drop(['isFraud'], axis=1)
+    X = df.drop(['isFraud', 'isFlaggedFraud'], axis=1)
     X = X.drop(['nameDest', 'nameOrig', 'step'], axis=1)
     X = pd.get_dummies(X)
     y = df['isFraud']
