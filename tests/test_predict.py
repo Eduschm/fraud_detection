cd31a7c18729ce05b698eac0d191c2838510067a
Eduardo schmegel <eschmegel@hotmail.com>
2025-11-28 00:04:02 -0300

    fix: CI

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,8 +2,10 @@ import pytest
 from src.predict import ModelPredictor
 from src.load_data import load_data
 
+
+# TO DO -- test the evaluate_models method using original data
 def test_evaluate_models():
-    X_test, y_test = load_data(data_path="data/sample_data.csv")
+    X_test, y_test = load_data(data_path="data/sample_data.csv", predict=True)
 
     model_predictor = ModelPredictor()
     results = model_predictor.evaluate_models(X_test, y_test)
