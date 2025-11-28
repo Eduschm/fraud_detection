5683de43dbce850f24b76571b545c3518b9b3485
Eduardo schmegel <eschmegel@hotmail.com>
2025-11-27 23:08:23 -0300

    fix> load data

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 # Load fraud data from CSV and drop isFraud,isFlaggedFraud
-def load_data(file_path):
+def load_data(file_path, predict=False):
+    if predict:
+        X = X.drop(['nameDest', 'nameOrig', 'step'], axis=1)
+        X = pd.get_dummies(X)
+        return X
     df = pd.read_csv('data/fraud_data.csv')
     X = df.drop(['isFraud', 'isFlaggedFraud'], axis=1)
     X = X.drop(['nameDest', 'nameOrig', 'step'], axis=1)
