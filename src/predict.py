27af91494ae7e1f3be2081a7d034f3f70c1d0aa9
Eduardo <eschmegel@hotmail.com>
2025-12-01 19:18:54 -0300

    feat: refactored methods

--- src/predict.py
+++ src/predict.py
@@ -42,28 +42,45 @@ class ModelPredictor():
         for file in os.listdir('models'):
             # Load model
             model = joblib.load(f"models/{file}")
-            # Predicts with trained model
-            y_pred = model.predict(X_test)
-            # Calculate metrics
             log.info(f"Calculating metrics for model: {file}")
-            f1 = f1_score(y_test, y_pred)
-            recall = recall_score(y_test, y_pred)
-            report = classification_report(y_test, y_pred)
-            conf_matrix = confusion_matrix(y_test, y_pred)
-            precision = precision_score(y_test, y_pred)
+            self.predict_model(model, X_test, y_test, file)
+            return self.results
 
-            # Store results
-            self.results[file.replace('.pkl', '')] = {
-                'Recall': recall,
-                'f1': f1,
-                'Precision': precision,
-                'classification_report': report,
-                'confusion_matrix': conf_matrix
-            }
+            
 
-            # Print accuracy
-            print(f"{file.replace('.pkl', '')} Recall: {recall:.4f}, f1: {f1}")
-            print(conf_matrix)
-            print(report)
+    def predict_model(self, model, X_test, y_test, file):
+        """Predict using a specific model.
+        Args:
+            model_name (str): Name of the model file (e.g., 'XGBClassifier.pkl').
+            X (DataFrame): Input features for prediction.
+            y_test (Series): True labels for the test data.
+            file: str
+                Model file name.
+        Returns:
+            array: Predicted labels.
+        """
+        # Predicts with trained model, adjusting the threshold to 0.3
+        y_pred = model.predict(X_test)
+        # Calculate metrics
+        
+        f1 = f1_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+
+        # Store results
+        self.results[file.replace('.pkl', '')] = {
+            'Recall': recall,
+            'f1': f1,
+            'Precision': precision,
+            'classification_report': report,
+            'confusion_matrix': conf_matrix
+        }
+
+        # Print accuracy
+        print(f"{file.replace('.pkl', '')} Recall: {recall:.4f}, f1: {f1}")
+        print(conf_matrix)
+        print(report)
         return self.results
         
